8c508fb1b5eac3b6c2a4863f90a464d5c497feb2
Percy0601 <2008dreams@2008.sina.com>
2016-05-20 16:34:04 +0800

    初始化项目

---
+++ log-appender-log4j/src/main/java/io/log/log4j/App.java
@@ -0,0 +1,12 @@
+package io.log.log4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+//        int i = 1_000;
+        System.out.println( "Hello World!" );
+    }
+}
